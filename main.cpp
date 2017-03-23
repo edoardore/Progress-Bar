48d9e13d74444f9d2950b6c8ecd448eab18ba487
edoardore <edoardore@me.com>
2017-03-23 11:06:47 +0100

    Il main adesso crea delle risorse (con la funzione carica) che servono per la simulazione della progressbar.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include "FileManage.h"
 
-
-
+void carica(FileManage *ptr) {
+    Risorse *risorsa[5];
+    risorsa[0] = new Risorse("Risorsa_1", 4);
+    risorsa[1] = new Risorse("Risorsa_02", 8);
+    risorsa[2] = new Risorse("Risorsa_03", 16);
+    risorsa[3] = new Risorse("Risorsa_04", 4);
+    risorsa[4] = new Risorse("Risorsa_05", 4);
+}
 
 int main() {
-   //TODO simulare creazione di file e il loro caricamento
+    FileManage *ptr = new FileManage;
+    carica(ptr);
 }
