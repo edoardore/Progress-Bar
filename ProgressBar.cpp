2dc3f8835d66c0edf427a333862c2a902e79efea
edoardore <edoardore@me.com>
2017-04-06 09:55:36 +0200

    Inizializzata la finestra della barra.

--- ProgressBar.cpp
+++ ProgressBar.cpp
@@ -3,11 +3,19 @@
 //
 
 #include "ProgressBar.h"
+#include <string>
+#include <ncurses.h>
 
+//inizializzazione della finestra per la barra
 ProgressBar::ProgressBar(FileManage *s) : Grafica::Grafica(s) {
-    //TODO utilizzo di ncurses
+    initscr();
+    curs_set(0);
+    noecho();
+    progressbar = newwin(10, 114, 17, 22);
+    setBUp(0);
+    setFUp(0);
+    setCurrentName("Unknown");
 }
-
 
 ProgressBar::~ProgressBar() {
     detach();
